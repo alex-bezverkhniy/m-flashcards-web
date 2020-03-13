41192263ba0a9aba2786d70192319fd718789f1d
Alexandr Bezverkhniy <alexandr.bezverkhniy@usbank.com>
2020-03-13 16:49:09 -0500

    Added cors settings

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // server.js
 var express = require('express');
+var cors = require('cors')
 // var path = require('path');
 var serveStatic = require('serve-static');
 var app = express();
+
+// var corsOptions = {
+//     origin: 'https://m-flashcards-api.herokuapp.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
+app.use(cors())
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
